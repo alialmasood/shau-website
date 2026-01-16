8e9de681d4a1dec5d184378a4353b796f3b0f00d
NARUTO <ali.unido@gmail.com>
2026-01-16 03:22:08 +0300

    db: init prisma migrations

---
+++ prisma/migrations/20260116002103_init/migration.sql
@@ -0,0 +1,67 @@
+-- CreateTable
+CREATE TABLE "contact_messages" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "full_name" VARCHAR(120) NOT NULL,
+    "email" VARCHAR(120) NOT NULL,
+    "phone" VARCHAR(30),
+    "subject" VARCHAR(200),
+    "message" TEXT NOT NULL,
+    "status" VARCHAR(20) NOT NULL DEFAULT 'new',
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "contact_messages_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "news" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "title" VARCHAR(250) NOT NULL,
+    "content" TEXT NOT NULL,
+    "is_published" BOOLEAN NOT NULL DEFAULT false,
+    "publish_date" TIMESTAMPTZ(6),
+    "created_by" UUID,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "news_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "user_id" UUID NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires_at" TIMESTAMPTZ(6) NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "username" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(120) NOT NULL,
+    "password_hash" TEXT,
+    "password_plain" TEXT,
+    "role" VARCHAR(20) NOT NULL DEFAULT 'user',
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions_token_key" ON "sessions"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "news" ADD CONSTRAINT "news_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
